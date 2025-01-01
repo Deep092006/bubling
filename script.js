446887524c8b1b64cc7b17872c6ed38846ecc633
Your Name <you@example.com>
2025-01-01 15:31:30 +0530

    javascript file without bulbing

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let div = document.querySelector("div");
 let ul = document.querySelector("ul");
 let li = document.querySelector("li");
 div.addEventListener("click", function (e) {
-  
+  e.stopPropagation();// stop event bulbing when clicking parent not reacts
     console.log("div pressed");
 });
 ul.addEventListener("click", function (e) {
+    e.stopPropagation();// stop event bulbing when clicking parent not reacts
   console.log("ul pressed");
 });
 li.addEventListener("click", function (e) {
+    e.stopPropagation();// stop event bulbing when clicking parent not reacts
   console.log("li pressed");
+
 });
